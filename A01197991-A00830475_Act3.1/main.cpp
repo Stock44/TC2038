03d78de49538a57433e5ae081eb8cee4be37a6a1
Hiram Muñoz <hirammunozr@outlook.com>
2023-03-27 22:20:07 -0600

    fixed minor issues

--- A01197991-A00830475_Act3.1/main.cpp
+++ A01197991-A00830475_Act3.1/main.cpp
@@ -2,59 +2,71 @@
 #include <vector>
 
 int ctanodos = 0;
+
 class nodeTrie {
 public:
     bool getEnd() {
         return end;
     };
+
     void setEnd(bool valuereceived) {
         end = valuereceived;
     };
+
     char getLetter() {
         return letter;
     };
-    nodeTrie(char c, nodeTrie* p) : parent(p), letter(c), end(false) {
-        for(int i=0; i<26; i++) {
+
+    nodeTrie(char c, nodeTrie *p) : parent(p), letter(c), end(false) {
+        for (int i = 0; i < 26; i++) {
             child[i] = nullptr;
         }
     }
-    nodeTrie* getNode(int i) {
+
+    nodeTrie *getNode(int i) {
         return child[i];
     };
-    nodeTrie* getChild(char c) {
+
+    nodeTrie *getChild(char c) {
         return child[c - 'a'];
     }
-    void addChild(nodeTrie* node) {
+
+    void addChild(nodeTrie *node) {
         child[node->letter - 'a'] = node;
     }
+
 private:
     char letter;
     bool end;
-    nodeTrie* parent;
-    nodeTrie* child[26];
+    nodeTrie *parent;
+    nodeTrie *child[26];
 };
+
 class trie {
 public:
     trie() {
         root = new nodeTrie('\0', nullptr);
     }
+
     void insert(std::string word);
+
     bool search(std::string word);
-    nodeTrie* getRoot() {
+
+    nodeTrie *getRoot() {
         return root;
     };
 private:
-    nodeTrie* root;
+    nodeTrie *root;
 };
+
 void trie::insert(std::string word) {
-    nodeTrie* current = root;
+    nodeTrie *current = root;
     for (int i = 0; i < word.length(); i++) {
         char c = word[i];
-        nodeTrie* sub = current->getChild(c);
+        nodeTrie *sub = current->getChild(c);
         if (sub != nullptr) {
             current = sub;
-        }
-        else {
+        } else {
             current->addChild(new nodeTrie(c, current));
             current = current->getChild(c);
         }
@@ -63,11 +75,12 @@ void trie::insert(std::string word) {
         }
     }
 }
+
 bool trie::search(std::string word) {
-    nodeTrie* current = root;
+    nodeTrie *current = root;
     for (int i = 0; i < word.length(); i++) {
         char c = word[i];
-        nodeTrie* sub = current->getChild(c);
+        nodeTrie *sub = current->getChild(c);
         if (sub == nullptr) {
             return false;
         }
@@ -75,41 +88,44 @@ bool trie::search(std::string word) {
     }
     return current->getEnd();
 }
-void printTriePh(int padre, nodeTrie* node) {
+
+void printTriePh(int padre, nodeTrie *node) {
     if (node == nullptr) {
         return;
     }
     ctanodos++;
     int actual;
     actual = ctanodos;
-    std::cout << "nodo padre: " << padre << ", nodo actual: " << actual << ",letra actual: " << node->getLetter() << std::endl;
+    std::cout << "nodo padre: " << padre << ", nodo actual: " << actual << ",letra actual: " << node->getLetter()
+              << std::endl;
     for (int i = 0; i < 26; i++) {
         if (node->getNode(i) != nullptr) {
             printTriePh(actual, node->getNode(i));
         }
     }
 }
-int main(){
+
+int main() {
     trie myTrie;
     int n;
     std::string word;
     std::vector<bool> isInTrie;
 
     std::cin >> n;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         std::cin >> word;
         myTrie.insert(word);
     }
     std::cin >> n;
-    for (int j = 0; j < n; j++){
+    for (int j = 0; j < n; j++) {
         std::cin >> word;
         isInTrie.push_back(myTrie.search(word));
     }
     printTriePh(0, myTrie.getRoot());
-    for (int k = 0; k < isInTrie.size(); k++){
-        std::cout << isInTrie[k] << " ";
+    for (int k = 0; k < isInTrie.size(); k++) {
+        std::cout << (isInTrie[k] ? "true\n" : "false\n");
     }
-    std::cout << std::endl;
+    std::cout.flush();
     return 0;
 
 }
