dc54b8728bedceb89a94ca1ea137902c8eaad61d
A00830475 <102173232+Angelrggarcia@users.noreply.github.com>
2023-04-14 07:19:52 -0600

    Update main.cpp

--- A01197991-A00830475_Act3.3/main.cpp
+++ A01197991-A00830475_Act3.3/main.cpp
@@ -1,3 +1,12 @@
+/**
+ * Actividad 3.3 Implementación de "Knapsack problem"
+ * Hiram Maximiliano Muñoz Ramirez A01197991
+ * Angel Rigoberto García García A00830475
+ * Este programa implementa un algoritmo de optimizacion combinatoria: el algoritmo Knapsack problem.
+ * 10/04/23
+ */
+
+
 #include <vector>
 #include <iostream>
 
@@ -6,9 +15,17 @@ struct Object {
     int weight;
 };
 
+/**
+ * Este algoritmo calcula la ganancia optima que se puede obtener al colocar objetos de distinto peso y valor dentro de
+ * un espacio limitado. Este algoritmo se ejecuta en tiempo O(nP), con n siendo el numero de articulos y P siendo
+ * el peso maximo.
+ * @param objects Un vector de objetos que contiene su informacion.
+ * @param maxWeight Peso maximo que soporta el contenedor.
+ * @return El valor de la ganancia maxima que se pudo obtener.
+ */
 int knapsackSolver(std::vector<Object> const &objects, int maxWeight) {
     std::size_t objectCount = objects.size();
-    std::vector<std::vector<int>> matrix(objectCount + 1, {maxWeight + 1});
+    std::vector<std::vector<int>> matrix(objectCount + 1, std::vector<int>(maxWeight + 1));
 
     for (int i = 1; i <= objectCount; i++) {
         for (int j = 1; j <= maxWeight; j++) {
@@ -17,30 +34,33 @@ int knapsackSolver(std::vector<Object> const &objects, int maxWeight) {
             } else {
                 matrix.at(i).at(j) = std::max(matrix.at(i - 1).at(j), objects.at(i - 1).value +
                                                                       matrix.at(i - 1).at(
-                                                                              j - objects.at(i - 1).weight));
+                                                                               j - objects.at(i - 1).weight));
             }
         }
     }
-    return *matrix.end()->end();
+    return *((matrix.end() - 1)->end() - 1);
 }
 
 int main() {
-    int n, values, maxWeight;
-    std::cin>>n;
+    std::size_t n;
+    std::cin >> n;
     std::vector<Object> products;
+    products.reserve(n);
+
     Object newObject{};
+    int value;
     for(int i = 0;i < n ;i++){
-        std::cin>>values;
-        newObject.value = values;
+        std::cin >> value;
+        newObject.value = value;
         products.push_back(newObject);
     }
     for(int j = 0;j < n ;j++){
-        std::cin>>values;
-        products.at(j).weight = values;
+        std::cin >> value;
+        products.at(j).weight = value;
     }
-    std::cin>>maxWeight;
-    std::cout << "El beneficio optimo es de: ";
-    std::cout << knapsackSolver(products, maxWeight) << std::endl;
 
+    int maxWeight;
+    std::cin >> maxWeight;
+    std::cout << knapsackSolver(products, maxWeight) << std::endl;
     return 0;
 }
